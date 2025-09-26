9def176b62720b53656bb5c92008d15f609b35eb
Lethcia Lira <lethiciasouza126@gmail.com>
2025-09-26 19:43:48 -0400

    Update breakout.py

--- breakout.py
+++ breakout.py
@@ -21,29 +21,62 @@ COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 
 # Constants
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 860
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 900
 PADDLE_WIDTH = 25
 PADDLE_HEIGHT = 120
 BALL_SIZE = 10
 
 # Screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("MyPong")
+pygame.display.set_caption("BREAKOUT")
 
 clock = pygame.time.Clock()
 
-#class Paddle:
+class Paddle:
+    def __init__(self):
+        self.rect = pygame.Rect(SCREEN_WIDTH// 2 - PADDLE_HEIGHT // 2, SCREEN_HEIGHT - 40, PADDLE_HEIGHT, PADDLE_WIDTH)
+        self.speed = 8
+
+    def move (self, keys):
+        if keys[pygame.K_LEFT] and self.rect.left >0:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT] and self.rect.right<SCREEN_WIDTH:
+            self.rect.x += self.speed
+    def draw(self, surface):
+        pygame.draw.rect(surface, COLOR_WHITE, self.rect)        
 
 #class Ball:
+    def __init__(self):
+        self.rect = pygame.Rect(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, BALL_SIZE , BALL_SIZE)
+        self.speed_x = 4
+        self.speed_y = -4
+    def move(self):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
 
+        if self.rect.left <= 0  or self.rect.right>= SCREEN_WIDTH:
+            self.speed_x *= -1
+            bounce_sound.play()
+        
+        if self.rect.top <=0 :
+            self.speed_y *= -1
+            bounce_sound.play()
+
+        def check_collision(self, paddle):
+            if self.rect.colliderect(paddle.rect):
+                self.speed_y *= -1
+                bounce_sound.play()
+        
+            def draw(self, surface):
+                pygame.draw.rect(surface, COLOR_WHITE, self.rect)
 
 # Function for menu
 def show_menu():
     waiting = True
     while waiting:
         screen.fill(COLOR_BLACK)
-        title_text = menu_font.render("MYPONG", True, COLOR_WHITE)
+        title_text = menu_font.render("BREAKOUT", True, COLOR_WHITE)
         start_text = score_font.render("Press SPACE to Play", True, COLOR_WHITE)
 
         screen.blit(title_text, (SCREEN_WIDTH//2 - title_text.get_width()//2, 200))
